ce15c38a79a78b80c56207a7c3307b6b07ca9ab7
sajedul5 <54474180+sajedul5@users.noreply.github.com>
2021-04-15 23:00:20 +0600

    templete design

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
 dotenv.config({path: 'config.env'});
 const port = process.env.PORT || 8080 ;
 
+//log requests
+app.use(morgan('tiny'));
+
+//parse request to body-parser
+app.use(bodyparser.urlencoded({extended:true}));
+
+
+//set view engine
+app.set('view engine', 'ejs');
+//app.set('views', path.resolve(__dirname, 'views/ejs'));
+
+//load assets
+app.use('/css', express.static(path.resolve(__dirname, 'asstes/css')));
+app.use('/img', express.static(path.resolve(__dirname, 'asstes/img')));
+app.use('/js', express.static(path.resolve(__dirname, 'asstes/js')));
+
 app.get('/', (req,res) =>{
-    res.send('CRUD Application');
+    res.render('index');
 });
 
 
